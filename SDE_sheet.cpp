8572a7ff306cb73446abbede2852fb2dcff86af0
Rahul karad <52450149+karad1818@users.noreply.github.com>
2021-11-19 00:01:23 +0530

    Update SDE_sheet.cpp

--- SDE_sheet.cpp
+++ SDE_sheet.cpp
@@ -214,3 +214,41 @@ void setZeroes(vector<vector<int>>& matrix) {
           matrix[0][j] = 0;
   }
 }
+
+
+// find in sorted rotated array
+
+// first find min number so that you can figureout how many times array is rotated
+// then simply do the BS
+int search(vector<int>& nums, int target) {
+  int n = (int)nums.size();
+  int l = 0,r = n-1;
+  while(l<r){
+      int mid = (l+r)/2;
+
+      if(nums[mid] > nums[r]){
+          // 4 5 6 1 2 3
+          l = mid + 1;
+      }else{
+          r = mid;
+      }
+  }
+
+  int rot = l;
+
+  l = 0 , r = n-1;
+  while(l<=r){
+      int mid = (l+r)/2;
+      int real_mid = (mid + rot)%n;
+      if(nums[real_mid] == target){
+          return real_mid;
+      }
+      if(nums[real_mid] > target){
+          r = mid - 1;
+      }else{
+          l = mid + 1;
+      }
+  }
+  return -1;
+}
+
